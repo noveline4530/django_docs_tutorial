70770761a921425e23b11613de5bc463f7d855cc
noveline4530 <sjun4530@gmail.com>
2020-02-15 19:49:06 +0900

    Commit before using a generic view

--- polls/models.py
+++ polls/models.py
@@ -17,6 +17,7 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
         
 class Choice(models.Model):
+    objects = models.Manager()
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
